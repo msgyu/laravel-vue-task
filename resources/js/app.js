bb62231861a9d980a2e43e3af5a362716692ba23
nousi <nousidayo@gmail.com>
2020-10-25 22:59:33 +0900

    import HeaderComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
+import HeaderComponent from ".components/HeaderComponent";
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +21,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component("header-component", HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +30,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app"
 });
