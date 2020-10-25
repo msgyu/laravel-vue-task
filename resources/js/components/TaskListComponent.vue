92b808a211648dde4c2d93063168563a2eab7fb8
nousi <nousidayo@gmail.com>
2020-10-25 23:25:56 +0900

    Add table in TaskListCOmponent

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+  <div class="container">
+    <table class="table table-hover">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">#</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
 
 <script>
 export default {};
