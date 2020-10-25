6800f564bc3a1ad4d692820516b737156b45a86c
nousi <nousidayo@gmail.com>
2020-10-25 22:53:53 +0900

    Add navbar in HeaderComponent.vue

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container-fluid bg-dark md-3">
+    <div class="container">
+      <nav class="navbar navbar-dark">
+        
+      </nav>
+    </div>
+  </div>
+</template>
