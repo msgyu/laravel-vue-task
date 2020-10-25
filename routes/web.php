1e61a67200e34ceac5c293b8848091aad379ce5b
nousi <nousidayo@gmail.com>
2020-10-25 22:50:37 +0900

    Add ->where('any', '.*') in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any}', function () {
-    return view('app');
-});
+    return view('layouts/app');
+})->where('any', '.*');
 
 Auth::routes();
 
