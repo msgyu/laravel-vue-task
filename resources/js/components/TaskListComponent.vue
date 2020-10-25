ab9802a28859443b278161d58f5f1443c1d6d128
nousi <nousidayo@gmail.com>
2020-10-25 23:27:05 +0900

    Add th

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -4,6 +4,12 @@
       <thead class="thead-light">
         <tr>
           <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Content</th>
+          <th scope="col">Person In Charge</th>
+          <th scope="col">Show</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
         </tr>
       </thead>
     </table>
