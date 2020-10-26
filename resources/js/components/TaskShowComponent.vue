a3312386db7266d7a75b2d62a85528923a5bdf00
nousi <nousidayo@gmail.com>
2020-10-26 23:23:24 +0900

    Add wrapper in TaskShowComponent

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -1,4 +1,12 @@
-<template></template>
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
