cd098a3e0871724eb69bd62a517e9725806f70e4
nousi <nousidayo@gmail.com>
2020-10-27 00:40:20 +0900

    Add container in TaskEdit

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-sm-6"></div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
