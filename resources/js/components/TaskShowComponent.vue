22e32ec92ee2c9b43be89d18b4ceebf28fb01238
nousi <nousidayo@gmail.com>
2020-10-26 23:25:18 +0900

    Add input of taskId

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -2,12 +2,27 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        
+        <form>
+          <div class="form-group row border-bottom">
+            <label for="id" class="col-sm-3 col-form-label">ID</label>
+            <input
+              type="text"
+              class="col-sm-9 form-control-plaintext"
+              readonly
+              id="id"
+              v-bind:value="taskId"
+            />
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    taskId: String
+  }
+};
 </script>
